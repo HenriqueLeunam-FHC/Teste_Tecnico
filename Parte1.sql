845992e345b1f8ebef85030387ac7f2db22b40e1
Henrique Leunam Silva <henrique.leunam@hotmail.com>
2025-10-24 11:05:14 -0300

    Desenvolvimento Parte 1

--- Parte1.sql
+++ Parte1.sql
@@ -20,26 +20,36 @@
 
 -- 1. Liste os 10 produtos mais vendidos em número de itens no ano de 2025.
 SELECT
-  EXTRACT(YEAR FROM o.order_date) AS ano_pedido,
   COALESCE(p.short_name, p.product_name) AS nome_produto,
-  SUM(oi.quantity) AS total_vendido,
-FROM `karhub-techtest.ecomm.products` p
-JOIN `karhub-techtest.ecomm.order_items` oi ON p.product_id = oi.product_id
-JOIN `karhub-techtest.ecomm.orders` o ON oi.order_id = o.order_id
-WHERE EXTRACT(YEAR FROM(o.order_date)) = 2025
+  SUM(oi.quantity) AS total_vendido
+FROM (
+    SELECT 
+        order_id,
+        order_date,
+        ROW_NUMBER() OVER (PARTITION BY order_id ORDER BY order_ts DESC) AS rn
+    FROM `karhub-techtest.ecomm.orders`
+) o
+JOIN `karhub-techtest.ecomm.order_items` oi 
+  ON o.order_id = oi.order_id
+JOIN `karhub-techtest.ecomm.products` p 
+  ON oi.product_id = p.product_id
+WHERE EXTRACT(YEAR FROM(o.order_date)) = 2025 AND o.rn = 1
 GROUP BY COALESCE(p.short_name, p.product_name), EXTRACT(YEAR FROM o.order_date)
 ORDER BY total_vendido DESC
+LIMIT 10
 
 -- 2. Liste os 5 clientes com maior gasto total no ano de 2025.
-SELECT 
-    INITCAP(LOWER(TRIM(c.customer_name))) AS nome_cliente,
-    EXTRACT(YEAR FROM o.order_date) AS ano,
-    SUM(oi.line_amount) AS gasto_total
+SELECT DISTINCT
+  INITCAP(LOWER(TRIM(c.customer_name))) AS nome_cliente,
+  SUM(SUM(oi.line_amount)) OVER (PARTITION BY c.customer_id) AS gasto_total
 FROM `karhub-techtest.ecomm.customers` c
-JOIN `karhub-techtest.ecomm.orders` o ON c.customer_id = o.customer_id
-JOIN `karhub-techtest.ecomm.order_items` oi ON o.order_id = oi.order_id
+JOIN `karhub-techtest.ecomm.orders` o
+  ON c.customer_id = o.customer_id
+JOIN `karhub-techtest.ecomm.order_items` oi
+  ON o.order_id = oi.order_id
 WHERE EXTRACT(YEAR FROM o.order_date) = 2025
-GROUP BY INITCAP(LOWER(TRIM(c.customer_name))), EXTRACT(YEAR FROM o.order_date)
+GROUP BY c.customer_id, c.customer_name, o.order_id, o.order_ts
+QUALIFY ROW_NUMBER() OVER (PARTITION BY o.order_id ORDER BY o.order_ts DESC) = 1
 ORDER BY gasto_total DESC
 LIMIT 5;
 
@@ -48,16 +58,30 @@ LIMIT 5;
 -- Responda com a query e com a justificativa para seleção desses produtos. 
 
 
-
-
-
-
-
-
-
-
 -- 4.  Considerando a região dos clientes, quais insights sobre logística você pode gerar?
+SELECT 
+  c.region AS regiao,
+  COUNT(DISTINCT o.order_id) AS qtd_compras,
+  SUM(o.total_amount) AS valor_total_compras,
+  COUNT(DISTINCT c.customer_id) AS total_clientes
+FROM 
+  `karhub-techtest.ecomm.customers` c
+JOIN 
+  `karhub-techtest.ecomm.orders` o ON c.customer_id = o.customer_id
+GROUP BY 
+  c.region
+ORDER BY 
+  total_compras DESC;
+
+/* Insights de logística
 
 -- 5.  Quais clientes deveriam ser incluídos em uma campanha de reativação? 
+
+-- A segmentação a ser considerada para uma campanha de reativação deve englobar clientes que já tiveram alguma compra
+e também clientes cadastrados porém não compradores.
+
+  -- 1. Compradores únicos: "Clientes que realizaram uma única compra"
+  -- 2. Clientes Compradores antigos: Cliente que compraram há mais de 60 dias e não compraram novamente.
+  -- 3. Clientes Inativos: Clientes que se cadastraram e demonstraram interesse, mas nunca efetuaram a primeira compra.
 
 -- 6.  Analisando a aquisição de novos clientes com base na data de cadastro, qual mês se destacou, indicando a possível eficácia de iniciativas de marketing?
