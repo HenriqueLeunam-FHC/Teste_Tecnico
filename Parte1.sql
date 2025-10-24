0cd161341f4609c11c9c5bcf25fa19bba827da76
Henrique Leunam Silva <henrique.leunam@hotmail.com>
2025-10-24 15:08:36 -0300

    Término do teste técnico

--- Parte1.sql
+++ Parte1.sql
@@ -53,10 +53,34 @@ QUALIFY ROW_NUMBER() OVER (PARTITION BY o.order_id ORDER BY o.order_ts DESC) = 1
 ORDER BY gasto_total DESC
 LIMIT 5;
 
-
 -- 3.  Quais produtos você sugere que recebam maior investimento em divulgação? 
 -- Responda com a query e com a justificativa para seleção desses produtos. 
+SELECT
+  COALESCE(p.short_name, p.product_name) AS nome_produto,
+  p.category as categoria,
+  COUNT(DISTINCT o.order_id) AS qtd_pedidos,
+  SUM(oi.quantity) AS total_unidades_vendidas,
+  ROUND(SUM(oi.line_amount), 2) AS receita_total,
+FROM `karhub-techtest.ecomm.products` p
+JOIN `karhub-techtest.ecomm.order_items` oi ON p.product_id = oi.product_id
+JOIN `karhub-techtest.ecomm.orders` o ON oi.order_id = o.order_id
+GROUP BY
+  p.product_id,
+  nome_produto,
+  p.category,
+  p.base_price
+ORDER BY
+  receita_total DESC
+LIMIT 10;
 
+-- Justificativa:
+
+-- A Query acima lista os produtos com maior receita total, número de pedidos e unidades vendidas.
+ -- Os produtos que geram maior receita e têm alta quantidade de pedidos indicam uma forte demanda. Investir na divulgação
+ -- desses produtos pode aumentar o resultado da empresa, aproveitando o interesse já existente dos clientes. Além disso,
+ -- o segundo grupo de maior investimento são os produtos com alta quantidade de unidades vendidas que embora não tenham receita
+ -- tão alta, indicam potencial para crescimento e fidelização de clientes.
+ 
 
 -- 4.  Considerando a região dos clientes, quais insights sobre logística você pode gerar?
 SELECT 
@@ -73,15 +97,84 @@ GROUP BY
 ORDER BY 
   total_compras DESC;
 
-/* Insights de logística
+-- Insights de Logística: 
+
+  -- 1. As regiôes com maior número de compras e pedidos estão no Norte e Centro-Oeste do país.
+     -- Essas regiões devem receber maior atenção logística para garantir a satisfação do cliente. Para isso, pode ser interessante
+     -- o investimento em melhorias no canal de distribuição, parcerias com transportadoras locais e melhoria nas rotas de entrega para aumentar a
+     -- velocidade de entrega e consequentemente a satisfação do cliente.
+
+  -- 2. Atualmente, as vendas nas regiões do país possuem a seguinte distribuição:
+
+    -- Norte + Nordeste = 38% do valor total de vendas e pedidos de compras
+    -- Centro-Oeste = 33% do valor total de vendas e 32% dos pedidos de compras.
+    -- Sul + Sudeste = 29% do valor total de vendas e 30% dos pedidos de compras.
+
+    -- Isso indica que as regiões ao Norte possuem maior consolidação das vendas e pedidos, enquanto as regiões ao Sul do país
+    -- possuem menor participação e pode ser alvo de ações estratégicas para aumentar a participação de mercado.
+
+    -- 3. Possíveis centros de distribuição no centro-oeste do país podem otimizar a logística para regiões Norte e Nordeste, reduzindo custos e 
+    -- melhorando prazos de entrega e margem de lucro.
+
 
 -- 5.  Quais clientes deveriam ser incluídos em uma campanha de reativação? 
 
--- A segmentação a ser considerada para uma campanha de reativação deve englobar clientes que já tiveram alguma compra
-e também clientes cadastrados porém não compradores.
-
   -- 1. Compradores únicos: "Clientes que realizaram uma única compra"
-  -- 2. Clientes Compradores antigos: Cliente que compraram há mais de 60 dias e não compraram novamente.
-  -- 3. Clientes Inativos: Clientes que se cadastraram e demonstraram interesse, mas nunca efetuaram a primeira compra.
+SELECT 
+    INITCAP(LOWER(TRIM(c.customer_name))) AS nome_cliente,
+    c.region AS regiao,
+    MAX(o.order_date) AS data_unica_compra,
+    o.total_amount AS valor_gasto,
+    STRING_AGG(DISTINCT COALESCE(p.short_name, p.product_name), ', ') AS produtos_comprados
+FROM `karhub-techtest.ecomm.customers` c
+JOIN (
+    SELECT *
+    FROM `karhub-techtest.ecomm.orders`
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY order_id ORDER BY order_ts DESC) = 1
+) o ON c.customer_id = o.customer_id
+JOIN `karhub-techtest.ecomm.order_items` oi ON o.order_id = oi.order_id
+JOIN `karhub-techtest.ecomm.products` p ON oi.product_id = p.product_id
+GROUP BY 
+    c.customer_id,
+    c.customer_name,
+    c.region,
+    o.total_amount
+HAVING COUNT(DISTINCT o.order_id) = 1
+ORDER BY data_unica_compra DESC;
+
+
+  -- 2. Clientes Compradores Antigos: Cliente que possuem a última compra há mais de 60 dias.
+SELECT 
+    INITCAP(LOWER(TRIM(c.customer_name))) AS nome_cliente,
+    c.region AS regiao,
+    MAX(o.order_date) AS ultima_compra,
+    DATE_DIFF(CURRENT_DATE(), MAX(o.order_date), DAY) AS dias_sem_compra
+FROM `karhub-techtest.ecomm.customers` c
+JOIN `karhub-techtest.ecomm.orders` o 
+    ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.customer_name, c.region
+HAVING DATE_DIFF(CURRENT_DATE(), MAX(o.order_date), DAY) > 60
+ORDER BY dias_sem_compra DESC;
+
+ -- 3. Clientes Inativos: Considerar base de clientes que nunca realizaram uma compra na loja.
 
 -- 6.  Analisando a aquisição de novos clientes com base na data de cadastro, qual mês se destacou, indicando a possível eficácia de iniciativas de marketing?
+SELECT 
+    CASE FORMAT_DATE('%m', sign_up_date)
+        WHEN '01' THEN 'Janeiro'
+        WHEN '02' THEN 'Fevereiro'
+        WHEN '03' THEN 'Março'
+        WHEN '04' THEN 'Abril'
+        WHEN '05' THEN 'Maio'
+        WHEN '06' THEN 'Junho'
+        WHEN '07' THEN 'Julho'
+        WHEN '08' THEN 'Agosto'
+        WHEN '09' THEN 'Setembro'
+        WHEN '10' THEN 'Outubro'
+        WHEN '11' THEN 'Novembro'
+        WHEN '12' THEN 'Dezembro'
+    END AS mes,
+    COUNT(customer_id) AS novos_cadastros
+FROM `karhub-techtest.ecomm.customers`
+GROUP BY mes, FORMAT_DATE('%m', sign_up_date)
+ORDER BY FORMAT_DATE('%m', sign_up_date);
